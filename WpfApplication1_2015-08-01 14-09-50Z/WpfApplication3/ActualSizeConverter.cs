9d44832e7eaee14f2c034f0c24f8c500ad6862ce
shartep <shartep@mail.ru>
2015-08-15 19:27:20 +0300

    15-08

---
+++ WpfApplication1_2015-08-01 14-09-50Z/WpfApplication3/ActualSizeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfApplication3
+{
+    class ActualSizeConverter : IMultiValueConverter
+    {
+
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return string.Format("{0:f2}; {1:f2}", values);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value.ToString().Split(';').Select(s => (object) System.Convert.ToDouble(s)).ToArray();
+        }
+    }
+}
---
+++ WpfApplication1_2015-08-01 14-09-50Z/WpfApplication3/BindingWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication3
+{
+    /// <summary>
+    /// Interaction logic for BindingWindow.xaml
+    /// </summary>
+    public partial class BindingWindow : Window
+    {
+        public BindingWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public enum BackgroundColors
+    {
+        OrangeRed,
+        YellowGreen,
+        BlueViolet,
+        Azure,
+        Gold
+    }
+
+    public class NameToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return ColorConverter.ConvertFromString(value.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
