a5f26de7240de8a7ff6994ba5f92f49326f366cf
shartep <shartep@mail.ru>
2015-10-10 19:22:27 +0300

    para3 10.10.15 final

---
+++ ASP/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            object c = this.Request.Form["checkbox1"];
+            return View();
+        }
+	}
+}
