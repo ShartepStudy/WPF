643917678aeb78b7c68a0c08097e2c80e9957cbc
shartep <shartep@mail.ru>
2015-10-31 16:37:31 +0200

    JqueryAJAX

---
+++ ASP/JqueryAJAX/JqueryAJAX/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JqueryAJAX.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ActionLink()
+        {
+            //return new ContentResult()
+            //{
+            //    Content = "<h5>hello from ActionLink</h5>"
+            //};
+            return PartialView("_Partial1", 2);
+        }
+
+        [HttpPost]
+        public ActionResult BeginForm(string name)
+        {
+            return new ContentResult()
+            {
+                Content = "<h5>hello " + name + " from ActionLink</h5>"
+            };
+        }
+    }
+}
